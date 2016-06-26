b3eadee586ce896f10d6972b5a72b4b6ac1b1948
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2016-06-26 15:31:56 -0500

    remaining changes pulled over from my poc - ImagePicker

--- MemeMe1.0/MemeMe1.0/ViewController.swift
+++ MemeMe1.0/MemeMe1.0/ViewController.swift
@@ -19,34 +19,129 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let memeTextAttribues = [
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 20)!,
+            NSStrokeWidthAttributeName: -1
+        ]
+        
         textFieldTop.delegate = self
         textFieldTop.text = "TOP"
         textFieldTop.textAlignment = .Center
+        textFieldTop.defaultTextAttributes = memeTextAttribues
         
         textFieldBottom.delegate = self
         textFieldBottom.text = "BOTTOM"
         textFieldBottom.textAlignment = .Center
+        textFieldBottom.defaultTextAttributes = memeTextAttribues
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        print("viewWillAppear")
+        
         setToolbarButtons()
         self.subscribeToKeyboardNotifications()
     }
-
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+        
+        self.unsubscribeFromKeyboardNotifications()
+    }
+    
     func setToolbarButtons() {
         if let cameraButton = toolBar.items![0] as? UIBarButtonItem {
             cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         }
     }
+    
+    @IBAction func pickAnImage(sender: AnyObject) {
+        print("pickAnImage")
         
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("imagePickerController")
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func subscribeToKeyboardNotifications() {
+        print("subscribeToKeyboardNotifications")
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        print("unsubscribeFromKeyboardNotifications")
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("imagePickerControllerDidCancel")
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        print("pickAnImageFromCamera")
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        print("textFieldDidBeginEditing")
+        
+        textField.text = "";
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        
+        return false;
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
+        
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        print("getKeyboardHeight")
+        
+        let userInfo = notification.userInfo!
+        let keyboardSize = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        print("keyboardWillHide")
+        
+        self.view.frame.origin.y = 0
     }
 }
