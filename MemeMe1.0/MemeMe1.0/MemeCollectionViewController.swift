e2d6f0f1ea0cac7d621b90e70e4a97213d60aa16
CURTIS MCMILLIN <curtismcmillin@CURTISs-iMac.local>
2016-08-06 11:53:23 -0500

    change initial vc, add collection vc

---
+++ MemeMe1.0/MemeMe1.0/MemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe1.0
+//
+//  Created by CURTIS MCMILLIN on 8/6/16.
+//  Copyright © 2016 CURTIS MCMILLIN. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UIViewController {
+    
+}
